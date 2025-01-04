aa3991af3d9ba2ff226aaf7129013b918a4ecdd1
DameerGamlet <gubaidulin.dam@gmail.com>
2025-01-04 18:58:28 +0400

    feat: git init react hospital app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+         <h1>Hospital</h1>
+         <h3>Дополнительная информация</h3>
+      </div>
+  )
+}
+
+export default App
